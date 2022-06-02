5f7d897204a3a9a29ab4ff430add87943dcfe0bc
anuzza <anuja3580@gmail.com>
2022-06-02 10:20:11 -0500

    added some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import AddUser from './Components/Users/AddUser';
+
 
 
 function App() {
+  
+  const addData = (props)=>{
+
+
+  }
   return (
     <div>
+      <AddUser  userData = {addData}/>
+      
 
     </div>
   );
