399eb4117dd792c8a40facd0eb22cc879bde5f23
anuzza <anuja3580@gmail.com>
2022-06-03 04:39:01 -0500

    added userslist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AddUser from './Components/Users/AddUser';
-
+import UsersList from './Components/Users/UsersList';
 
 
 function App() {
@@ -12,6 +12,8 @@ function App() {
   return (
     <div>
       <AddUser  />
+      <UsersList users = {[]}/>
+      
       
 
     </div>
